8705c570129d10d718ddea79452910bf6b58a1d7
Fanglanc <fanglanc@vt.edu>
2025-04-29 03:11:37 +0000

    Deploying to gh-pages from @ Fanglanc/Fanglanc.github.io@e3519670823de825558be1d462df40b8fe02b556 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-services",
+          title: "Services",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/services/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
