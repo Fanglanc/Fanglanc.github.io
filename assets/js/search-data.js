406b962ff1175195f96638dc2e4160790dd05158
Fanglanc <fanglanc@vt.edu>
2025-04-29 08:13:12 +0000

    Deploying to gh-pages from @ Fanglanc/Fanglanc.github.io@ba1e47bf32fac892836890947c154652e41ba966 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,13 +38,11 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-one-paper-has-been-accepted-by-the-sdm-2025-conference-fireworks",
+          title: 'One paper has been accepted by the SDM 2025 conference. :fireworks:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
